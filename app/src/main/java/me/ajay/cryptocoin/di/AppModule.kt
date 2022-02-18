cdbcca3870de60946414a2d10dc4860e691e2137
ajay.dewari <ajay.dewari@payufin.com>
2022-02-19 01:33:37 +0530

    AppModule added with the hilt dependency to get the object of CoinRepository

---
+++ app/src/main/java/me/ajay/cryptocoin/di/AppModule.kt
@@ -0,0 +1,34 @@
+package me.ajay.cryptocoin.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import me.ajay.cryptocoin.common.Constants
+import me.ajay.cryptocoin.data.remote.CoinPaprikaApi
+import me.ajay.cryptocoin.data.repo.CoinRepositoryImpl
+import me.ajay.cryptocoin.domain.repositories.CoinRepository
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun providePaprikaApi(): CoinPaprikaApi {
+        return Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(CoinPaprikaApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideCoinRepository(api: CoinPaprikaApi): CoinRepository {
+        return CoinRepositoryImpl(api)
+    }
+}
